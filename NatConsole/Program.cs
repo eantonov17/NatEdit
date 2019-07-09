d48e4c0c048bc91ef5824c61fb5ad04691347258
Eugene Antonov <github@antonovs.org>
2019-07-09 16:32:08 -0400

    Add project files.

---
+++ NatConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NatConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
